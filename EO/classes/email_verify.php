61544d71f2d521cbe03a6dee80e2c2402f213453
Yaxin Hu <huyaxxin@gmail.com>
2015-03-20 21:18:19 +0800

    email_verify class
    DB connection problem @aaaaikit

---
+++ EO/classes/email_verify.php
@@ -0,0 +1,93 @@
+<?php
+/*
+NEED "last_user ID","user email" in DB
+ALSO neet store "user_activation_hash" in DB in send()
+ 
+add lines in register.php before "Redirect::to('index.php');""
+
+                    $email_verification = new email_verify();
+                    $useremail=Input::get('username');
+                    $email_verification = email_verify->send($useremail);
+
+**/
+class email_verify{
+    private $_db = null;
+
+    public function __construct() {
+        $this->_db = DB::getInstance();
+    }
+
+    public function send($user_email){
+
+
+ 		$mail = new PHPMailer;
+
+        // please look into the config/config.php for much more info on how to use this!
+        // use SMTP or use mail()
+
+        // generate random hash for email verification (40 char string)
+        $user_activation_hash = sha1(uniqid(mt_rand(), true));
+
+        $username=$user_email;
+        $user_id = $user_id;
+ 
+
+        if (EMAIL_USE_SMTP) {
+            // Set mailer to use SMTP
+            $mail->IsSMTP();
+            //useful for debugging, shows full SMTP errors
+            //$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+            // Enable SMTP authentication
+            $mail->SMTPAuth = EMAIL_SMTP_AUTH;
+            // Enable encryption, usually SSL/TLS
+            if (defined(EMAIL_SMTP_ENCRYPTION)) {
+                $mail->SMTPSecure = EMAIL_SMTP_ENCRYPTION;
+            }
+            // Specify host server
+            $mail->Host = EMAIL_SMTP_HOST;
+            $mail->Username = EMAIL_SMTP_USERNAME;
+            $mail->Password = EMAIL_SMTP_PASSWORD;
+            $mail->Port = EMAIL_SMTP_PORT;
+        } else {
+            $mail->IsMail();
+        }
+
+        $mail->From = EMAIL_VERIFICATION_FROM;
+        $mail->FromName = EMAIL_VERIFICATION_FROM_NAME;
+        $mail->AddAddress($user_email);
+        $mail->Subject = EMAIL_VERIFICATION_SUBJECT;
+
+        $link = EMAIL_VERIFICATION_URL.'?id='.urlencode($user_id).'&verification_code='.urlencode($user_activation_hash);
+
+        // the link to your register.php, please set this value in config/email_verification.php
+        $mail->Body = EMAIL_VERIFICATION_CONTENT.' '.$link;
+
+        if(!$mail->Send()) {
+            $this->errors[] = MESSAGE_VERIFICATION_MAIL_NOT_SENT . $mail->ErrorInfo;
+            return true;
+        } else {
+            return true;
+        }
+
+    }	
+
+    public function verifyNewUser($user_id, $user_activation_hash)
+    {
+        // if database connection opened
+        if ($this->databaseConnection()) {
+            // try to update user with specified information
+            $query_update_user = $this->db_connection->prepare('UPDATE users SET user_active = 1, user_activation_hash = NULL WHERE user_id = :user_id AND user_activation_hash = :user_activation_hash');
+            $query_update_user->bindValue(':user_id', intval(trim($user_id)), PDO::PARAM_INT);
+            $query_update_user->bindValue(':user_activation_hash', $user_activation_hash, PDO::PARAM_STR);
+            $query_update_user->execute();
+
+            if ($query_update_user->rowCount() > 0) {
+                $this->verification_successful = true;
+                $this->messages[] = MESSAGE_REGISTRATION_ACTIVATION_SUCCESSFUL;
+            } else {
+                $this->errors[] = MESSAGE_REGISTRATION_ACTIVATION_NOT_SUCCESSFUL;
+            }
+        }
+    }
+}
+
