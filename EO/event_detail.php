ffbff6f3dd8ccc517c53431b552cb7d8323b9bda
Yaxin Hu <huyaxxin@gmail.com>
2015-04-03 17:53:16 +0800

    event_detail UI

---
+++ EO/event_detail.php
@@ -0,0 +1,34 @@
+<div class= "event-photo">
+	<class = img class="item-picture" src="images/cuhk-test.jpg">
+</div>
+
+<div class = "event-details">
+	<h1>Event-name:</h1>2015 - 04 - 22<br/>
+	<li>Date:</li>本部<br/>
+	<li>Time:</li>11:00 - 13:00<br/>
+	<li>Description:</li>香港中文大學，簡稱中大，是一所坐落於香港沙田的公立研究型大學。大學於1963年創校，並於1966年開辦香港首間研究院。其最初是由三所現有的書院合併而成，當中的源流最早可追索至1949年。其中的崇基學院和聯合書院，本身亦是由一些於清末至民國時期、在中國內地建立的教會及私立大學合併而成。<br/>
+</div>
+
+<div class = "like-comment">
+	<p1>LIKE | COMMENT</p1>
+</div>
+<div class = "icons">
+	<class = img class = "icon" scr = "images/cuhk-test.jpg" width = "42" height = "42">
+	<class = img class = "icon" scr = "images/cuhk-test.jpg">
+	<class = img class = "icon" scr = "images/cuhk-test.jpg">
+	<p>liked this</p>
+</div>
+
+<div class = "comment-content">
+	<div class = "user1">
+		<p>"comment_content"</p>
+	</div>
+
+	<div class = "user2">
+		<p>"comment_content"</p>
+	</div>
+
+	<div class = "user3">
+		<p>"comment_content"</p>
+	</div>	
+</div>
