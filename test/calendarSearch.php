f40dd58718414411e10fc35d97d345a9f36c0cf4
ls-morning <luosha109@gmail.com>
2015-04-08 21:21:05 +0800

    calendar

--- test/calendarSearch.php
+++ test/calendarSearch.php
@@ -5,12 +5,13 @@
 
 <div>
 
-<h2>Search by Calendar</h2>
 
-<div class="field">
-	<!-- <label for='password'>Time</label> -->
+
+<div class="field" align="center">
+	<h2>Search by Calendar - 
 	<input type="date" name="time" id="dateSearch">
 	<input type="button" value="Search" onclick="calendarSearch()">
+	</h2>
 </div>  
 
 
