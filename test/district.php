86f1962574fb264c7005c13c96543c3def47a05a
zxmeng <bluedreamermzx@gmail.com>
2015-04-08 18:02:56 +0800

    update district

--- test/district.php
+++ test/district.php
@@ -6,75 +6,84 @@
       session_start();
    }
 
-         // echo $district;
-
    if($flag == "event"){
       $et = new Event();
       $events = $et->getEventsByDistrict($district);
       $et->db->close();
-
-      foreach($events as $event){
+      
+      if(empty($events)) {
 ?>
-      <div class="item">
-            <div class="item-name">
-               <?php echo $event['title'] ?>
-            </div>
-
-            <div class="item-picture">
-               <img class="item-picture" src="<?php echo $event['ePhoto']; ?>" alt="file not found">
-            </div>
-
-            <div class="item-infomation">
-               <h2>Date:</h2> <?php echo $event['eDate']; ?><br/>
-               <!-- <h2>Time:</h2><?php echo $event['lastEditTime']; ?><br/> -->
-               <h2>Venue:</h2><?php echo $event['venue']; ?><br/>
-               <h2>Description:</h2><?php echo $event['eDescription']; ?><br/>
-            </div>
-
-            <div align="right">
-               <div class="button" type="button" onclick="loadEvent( <?php echo $event['eid'].', '.$_SESSION['id']; ?> )">
-                  More Infomation
+      <div align="center"><h2>No event in <?php echo $district; ?> now<br></h2></div>
+<?php
+      }
+      else {
+         foreach($events as $event){
+?>
+         <div class="item">
+               <div class="item-name">
+                  <?php echo $event['title'] ?>
                </div>
-            </div>               
-      </div>
-   <!--end 1 event--> 
+
+               <div class="item-picture">
+                  <img class="item-picture" src="<?php echo $event['ePhoto']; ?>" alt="file not found">
+               </div>
+
+               <div class="item-infomation">
+                  <h2>Date:</h2> <?php echo $event['eDate']; ?><br/>
+                  <!-- <h2>Time:</h2><?php echo $event['lastEditTime']; ?><br/> -->
+                  <h2>Venue:</h2><?php echo $event['venue']; ?><br/>
+                  <h2>Description:</h2><?php echo $event['eDescription']; ?><br/>
+               </div>
+
+               <div align="right">
+                  <div class="button" type="button" onclick="loadEvent( <?php echo $event['eid'].', '.$_SESSION['id']; ?> )">
+                     More Infomation
+                  </div>
+               </div>               
+         </div>
 <?php 
-      } 
+         } 
+      }
 
    }
 
    else if($flag == 'review'){
-      // echo $district;
 
       $re = new Review();
       $reviews = $re->getReviewsByDistrict($district);
       $re->db->close();
-
-      foreach($reviews as $review){
+      if(empty($reviews)) {
 ?>
-      <div class="item">
-            <div class="item-name">
-               <?php echo $review['title'] ?>
-            </div>
-
-            <div class="item-picture">
-               <img class="item-picture" src="<?php echo $review['ePhoto']; ?>" alt="file not found">
-            </div>
-
-            <div class="item-infomation">
-               <h2>Date:</h2> <?php echo $review['eDate']; ?><br/>
-               <!-- <h2>Time:</h2><?php echo $event['lastEditTime']; ?><br/> -->
-               <h2>Venue:</h2><?php echo $review['venue']; ?><br/>
-               <h2>Description:</h2><?php echo $review['eDescription']; ?><br/>
-            </div>
-
-            <div align="right">
-               <div class="button" type="button" onclick="loadReview( <?php echo $review['pid'].', '.$_SESSION['id']; ?> )">
-                  More Infomation
+      <div align="center"><h2>No review in <?php echo $district; ?> now<br></h2></div>
+<?php
+      }
+      else {
+         foreach($reviews as $review){
+?>
+         <div class="item">
+               <div class="item-name">
+                  <?php echo $review['title'] ?>
                </div>
-            </div>               
-      </div>
+
+               <div class="item-picture">
+                  <img class="item-picture" src="<?php echo $review['ePhoto']; ?>" alt="file not found">
+               </div>
+
+               <div class="item-infomation">
+                  <h2>Date:</h2> <?php echo $review['eDate']; ?><br/>
+                  <!-- <h2>Time:</h2><?php echo $event['lastEditTime']; ?><br/> -->
+                  <h2>Venue:</h2><?php echo $review['venue']; ?><br/>
+                  <h2>Description:</h2><?php echo $review['eDescription']; ?><br/>
+               </div>
+
+               <div align="right">
+                  <div class="button" type="button" onclick="loadReview( <?php echo $review['pid'].', '.$_SESSION['id']; ?> )">
+                     More Infomation
+                  </div>
+               </div>               
+         </div>
 <?php         
+         }
       }
 
    }
